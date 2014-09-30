74e0a136b1ba3fae44d7b3a571edf5a694229473
Ijeoma <gift_ij2002@yahoo.com>
2014-09-30 13:06:59 +0100

    complete and working. can be perfected

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,10 @@
 			$scope.header = true;
 			$scope.unavailable = false;
 			$scope.incorrect = false;
-			$scope.foodItems = [];
+			$scope.hideIngredients = true;
+			$scope.leavePresent = true;
+			$scope.rcpName = false;
+			$scope.onclick = true;
 		
 		$scope.imageProcess = function(imgUrl) {
 			if (imgUrl && imgUrl[90]) {
@@ -13,7 +16,24 @@
 			return 'images/unavailable.jpg'
 		}
 
+		$scope.showIngredients = function() {
+			this.hideIngredients = false;
+			this.rcpName = false;
+			this.leavePresent = false;
+			this.onclick = false;
+		}
+
+		$scope.HideIngredients = function() {
+			this.rcpName = false;
+			this.hideIngredients = true;
+			this.leavePresent = true;
+			this.onclick = true;
+		}
+
 		$scope.getSearch = function() {
+			$scope.foodItems = [];
+			$scope.numItems = "";
+			$scope.load = true;
 			var config = {
 				method: "jsonp",
 				url: "http://api.yummly.com/v1/api/recipes",
@@ -28,25 +48,29 @@
 				if (!isNaN($scope.typeIn)) {
 				 	$scope.header = false;
 				 	$scope.incorrect = true;
+				 	$scope.load = false;
 				 	$scope.numItems = false;
 				 	return false;
 				}
-
+				
 				$http(config).success(function(response) {
 					$scope.foodItems = response;
 					$scope.result = $scope.foodItems.matches;
-
+					
 					if ($scope.result.length < 1) {
 						$scope.header = false;
 						$scope.numItems = false;
 						$scope.unavailable = true;
+						$scope.load = false;
 					}	
 					else {
 						$scope.header = true;
 						$scope.numItems = true;
+						$scope.load = false;
 					}
 				});
 				$scope.typeIn = "";
+				
 		};
 	}]);
 })();
